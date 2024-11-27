206b92fb63bac14196e8b6a0e617091bf47a9b29
Maria luiza <up202304306@up.pt>
2024-11-27 08:40:26 +0000

    updated relational schema

--- Musicfy.sql
+++ Musicfy.sql
@@ -2,7 +2,7 @@
 CREATE TABLE Person
 (
 	userID NUMERIC UNIQUE, 
-	NAME  TEXT NOT NULL Constraint ,
+	NAME TEXT NOT NULL Constraint ,
     location TEXT, 
     profilePic TEXT, 
     PRIMARY KEY (userID)
@@ -16,7 +16,7 @@ CREATE TABLE Artist
     FOREIGN KEY (userID) REFERENCES Person(userID)
 );
 
-CREATE TABLE user
+CREATE TABLE User
 (
     userID NUMERIC,
     password blob NOT NULL Constraint,
@@ -100,36 +100,38 @@ CREATE TABLE Theme
 
 
 /*
-
 Pessoa (userID, nome, localizacao, fotoPerfil)
-
-Artista (biografia, proximosEventos)
-
-Utilizador (nomeUtilizador, password, contacto, dataNascimento)
-	PlanoAssinatura (assinatura)
-
-
-Criador (biografia)
-
-
-Playlist (playlistID, nome, descricao, dataCriacao, foto)
-
-Musica (musicaID, nome, duracaoSeg, numReproducoes, fotoCapa)
-
+Artista (userID -> Pessoa.userID, biografia, proximosEventos)
+Utilizador (userID -> Pessoa.userID, nomeUtilizador, password, contacto, dataNascimento, assinaturaID -> PlanoAssinatura)
+PlanoAssinatura (assinaturaID, assinatura)
+Criador (userID -> Pessoa.userID, biografia)
+Playlist (playlistID, nome, descricao, dataCriacao, foto, userID -> Utilizador)
+Musica (musicaID, nome, duracaoSeg, numReproducoes, fotoCapa, albumID -> Album)
 Album (albumID, nome, dataLancamento, foto)
-
 Podcast (podcastID, nome, foto)
-
-Episodio (episodioID, nome, descricao, dataLancamento, duracaoSeg)
-
-
+Episodio (episodioID, nome, descricao, dataLancamento, duracaoSeg, temaID -> Tema)
 Genero (genero)
-
 Tema (tema)
+—-------------------------------------------------------------------------------------------------------------------------
+OuviuMu (episodioID -> Episodio, userID -> Utilizador, timeStamp, progressoEpisodio )
+OuviuEp (musicaID -> Musica, userID -> Utilizador, timeStamp)
+—-------------------------------------------------------------------------------------------------------------------------
+Seguiu (userID1 -> Utilizador, userID2 -> Utilizador)
+Seguiu (userID -> Utilizador, userID -> Artista)
+Seguiu (userID -> Utilizador, userID -> Criador)
 
-—------------
+Gostou (userID -> Utilizador, albumID -> Album)
+Gostou (userID -> Utilizador, espisodioID -> Episodio)
+Gostou (userID -> Utilizador, musicaID -> Musica)
 
-OuviuMu (episodioID -> Episodio, nomeUtilizador -> Utilizador, timeStamp, progressoEpisodio )
-OuviuEp (musicaID -> Musica, nomeUtilizador -> Utilizador, timeStamp)
+Colaborador (userID -> Utilizador, playlistID -> Playlist)
+Guardou (userID -> Utilizador, playlistID -> Playlist)
 
-Seguiu (nomeUtilizador1 -> Utilizador, nomeUtilizador2 -> Utilizador)*/
+ArtistaMusica (userID -> Artista, musicaID -> Musica)
+MusicaPlaylist (musicaID -> Musica, playlistID -> Playlist)
+MusicaGenero (musicaID -> Musica, generoID -> Genero)
+PodcastGenero (musicaID -> Musica, podcastID -> Podcast)
+PodcastEpisodio ( podcastID -> Podcast, episodioID -> Episodio)
+CriadorPodcast (userID -> Criador, podcastID -> Podcast)
+
+*/
