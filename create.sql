e146b315f72e0f4d62ef1bfce43ddbf5be669ad8
Maria luiza <up202304306@up.pt>
2024-12-01 18:17:33 +0000

    referenciar nomeUtilizadorID e contactoID

--- create.sql
+++ create.sql
@@ -22,16 +22,16 @@ DROP TABLE IF EXISTS Utilizador;
 CREATE TABLE Utilizador
 (
     userID INT NOT NULL UNIQUE,
-    nomeUtilizador VARCHAR(25) NOT NULL UNIQUE CHECK (LENGTH(nomeUtilizador) > 3),
+    nomeUtilizadorID INT NOT NULL UNIQUE,
     palavrapasse VARCHAR(35) NOT NULL CHECK (LENGTH(palavrapasse) >= 8),
-    contacto VARCHAR(35) NOT NULL UNIQUE,
+    contactoID INT NOT NULL UNIQUE,
     dataNascimento DATE NOT NULL,
     assinaturaID INT NOT NULL UNIQUE,
     PRIMARY KEY (userID),
     FOREIGN KEY (userID) REFERENCES Pessoa(userID),
     FOREIGN KEY (assinaturaID) REFERENCES PlanoAssinatura(subscriptionID),
-    FOREIGN KEY (contacto) REFERENCES Contacto(contacto)
-    FOREIGN KEY (nomeUtilizador) REFERENCES NomeUtilizador(nomeUtilizador)
+    FOREIGN KEY (contactoID) REFERENCES Contacto(contactoID),
+    FOREIGN KEY (nomeUtilizadorID) REFERENCES NomeUtilizador(nomeUtilizadorID)
 );
 
 DROP TABLE IF EXISTS Contacto;
